e3950eaf28b88c546caabcce77e376f7e5269f9d
Shortink <21148306+Shortink@users.noreply.github.com>
2024-10-23 01:04:45 -0600

    flight manager class

---
+++ Data/Flight.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightReservation.Data
+{
+    public class Flight
+    {
+        string flightNumber;
+        string airline;
+        string origin;
+        string destination;
+        string day;
+        string time;
+        int temp; // idk what this is supposed to be
+        double cost;
+
+
+        public string FlightNumber { get => flightNumber; set => flightNumber = value; }
+        public string Airline { get => airline; set => airline = value; }
+        public string Origin { get => origin; set => origin = value; }
+        public string Destination { get => destination; set => destination = value; }
+        public string Day { get => day; set => day = value; }
+        public string Time { get => time; set => time = value; }
+        public int Temp { get => temp; set => temp = value; }
+        public double Cost { get => cost; set => cost = value; }
+
+        public Flight()
+        {
+        }
+
+        public Flight(string flightNumber, string airline, string origin, string destination, string day, string time, int temp, double cost)
+        {
+            this.flightNumber = flightNumber;
+            this.airline = airline;
+            this.origin = origin;
+            this.destination = destination;
+            this.day = day;
+            this.time = time;
+            this.temp = temp;
+            this.cost = cost;
+        }
+    }
+}
---
+++ Data/FlightManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightReservation.Data
+{
+    public class FlightManager
+    {
+        string airportPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\..\\..\\Resources\\res\\airports.csv");
+        string flightPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\..\\..\\Resources\\res\\flights.csv");
+
+        public static List<Flight> flights = new();
+
+        public FlightManager()
+        {
+            LoadFlights();
+        }
+
+        public void LoadFlights()
+        {
+            foreach (string line in File.ReadAllLines(flightPath))
+            {
+                string[] parts = line.Split(',');
+                string flightNumber = parts[0];
+                string airline = parts[1];
+                string origin = parts[2];
+                string destination = parts[3];
+                string day = parts[4];
+                string time = parts[5];
+                int temp = int.Parse(parts[6]);
+                double cost = double.Parse(parts[7]);
+
+                flights.Add(new Flight(flightNumber, airline, origin, destination, day, time, temp, cost));
+            }
+        }
+
+         // function should be able to use any combination of the 3 parameters to find and return flights, returns all flight if all input parameters are null
+        public static List<Flight> FlightFinder(string? origin, string? destination, string? day) 
+        {
+            List<Flight> validFlights = new List<Flight>();  //final list that will be returned
+            List<Flight> foundFlights = flights.FindAll(flight => flight.Origin == origin && flight.Destination == destination && flight.Day == day);  //need to add exception handle for null values
+            
+            return foundFlights;
+        }
+
+    }
+}
